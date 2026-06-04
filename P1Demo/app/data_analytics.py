6e7a1f7817df83fe25f14b758f1bc61329e79dd5
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-04 12:23:27 -0400

    barabar

--- P1Demo/app/data_analytics.py
+++ P1Demo/app/data_analytics.py
@@ -1,5 +1,6 @@
 import repositories.animal_repository as repo
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # NOTE: This will be WAY less convoluted once we're working with a real SQL database
 
@@ -14,9 +15,11 @@ data = {
     }
     for animal_id, animal in repo.animals.items()
 }
+
+print(data) # Not Python Animal objects anymore
+
 # "If using all scalar values, you must pass an index"
 df = pd.DataFrame.from_dict(data, orient="index")
-
 
 # I think we need to dissolve each animal object into standalone records, not objects
 # yup
@@ -28,4 +31,13 @@ average_rating = df["Guest Rating"].mean() # thx numpy
 
 print(average_rating)
 
-# Plot the animal ratings with a mean line------------
+# Plot the animal ratings with a mean bar------------
+
+plt.bar(df["Name"], df["Guest Rating"])
+
+plt.axhline(y=average_rating, color="red", linestyle="--")
+
+plt.title("Animal Guest Ratings")
+
+plt.show()
+
